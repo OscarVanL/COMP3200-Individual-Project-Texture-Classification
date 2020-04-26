1d1309ff74226bbe1295edebf07db7bae82f19c1
OscarVanL <7999692+OscarVanL@users.noreply.github.com>
2020-04-26 17:35:49 +0100

    Corrected variable name

--- algorithms/AlgorithmInterfaces.py
+++ algorithms/AlgorithmInterfaces.py
@@ -136,7 +136,7 @@ class NoiseClassifierInterface:
             self.dataset_y.append('gaussian')
             self.dataset_X.append(image.gauss_25_noise_featurevector)
             self.dataset_y.append('gaussian')
-            self.dataset_X.append(image.speckle_noise_featurevector)
+            self.dataset_X.append(image.speckle_002_noise_featurevector)
             self.dataset_y.append('speckle')
             self.dataset_X.append(image.speckle_004_noise_featurevector)
             self.dataset_y.append('speckle')
